d381ebc57f7505e234392f8aaec5857595cb8605
Meer Uzair <meer.uxair007@gmail.com>
2023-08-28 22:18:23 +0530

    updated css

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const miniPostLiked = document.getElementById("mini-post-liked");
 const loadMoreBtn = document.getElementById("loadMore");
 
 // Fetch main navigation data from a JSON file
-fetch("main-nav.json")
+fetch("/main-nav.json")
   .then((response) => response.json())
   .then((mainNavData) => {
     // Fetch sub-navigation data from a JSON file
@@ -78,7 +78,7 @@ function generateNavlinks(mainNavData, subNavData) {
 
 // Fetch card data from a JSON file
 function renderData() {
-  fetch("cards.json")
+  fetch("/cards.json")
     .then((res) => res.json())
     .then((data) => {
       // Call functions to render carousel and cards
